1a05fb2e0b3375c2b447bac8a6adbe2b1c929c78
李光浩 <liguanghao@viatomtech.com>
2021-05-31 15:05:00 +0800

    rpn go

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,11 @@
 #include <string>
 #include "table.h"
 #include <math.h>
+#include <android/log.h>
+#define  LOG_TAG    "your-log-tag"
 
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 #define isnumber(c)            ((c >= '0' && c <= '9') || c == '.')
 #define isvariable(c)            (c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e' || c == 'f')
@@ -155,36 +159,205 @@ FUNCTION function[] = {
 VARIABLE variable = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 EQUATION equation;
 
+char function_name[NAME_LENGTH];
 
-double parseEquation(double x, double y, double z) {
-    for (token = 0; token < equation.xyz_tokennumber; token++) {
-        switch (equation.xyz_token[token].type) {
-            case NUMBER_TYPE:
-                execution_stack[++stack_index] = equation.xyz_token[token].value;
-                break;
 
-            case UNKNOWN_TYPE:
-                if (equation.xyz_token[token].index == X) {
-                    execution_stack[++stack_index] = x;
-                } else if (equation.xyz_token[token].index == Y) {
-                    execution_stack[++stack_index] = y;
-                } else {
-                    execution_stack[++stack_index] = z;
+
+
+
+
+
+
+
+double i10powx(int a){
+    return pow((double)10,(double)a);
+}
+
+
+
+char *
+lower(char *t, const char *s)
+{
+    char *r = t;
+    while (*s)
+        *t++ = tolower(*s++) ;
+    *t = '\0';
+    return r;
+}
+
+
+
+
+//--------------------------------------这个是解析字符串常数的， 例如"2343.563e-2，    1423445346"
+
+double atof_local(const char * _buf) {
+    double val,expo;
+    int minus,fraction_digits;
+    double integral,ifraction;
+
+    char c,*__buf,*buf;
+
+    if( (__buf = (char *)  malloc((strlen(_buf) + 1) * sizeof(char)) ) == NULL )
+        return 0;
+
+    buf = lower(__buf,_buf); // just handle lowercase chars
+
+    integral = ifraction = 0;
+    fraction_digits = 0;
+    val = 0.0;
+    expo = 1.0;
+
+    if((minus = (*buf == '-')))
+        buf++;
+    if (*buf == '+')
+        buf++;
+
+    while (*buf && *buf != '.') {
+        c = *buf++;
+        if (isdigit(c))
+            integral = 10 * integral + c - '0';
+        else {
+
+            if (c == 'e') {
+                buf--;
+                goto parse_expo;
+            }
+            goto finish;
+        }
+    }
+
+
+    if (*buf == '.') {
+        buf++;
+        while (*buf && *buf != 'e' ) {
+            c = *buf++;
+            if (! isdigit(c)) goto finish;
+            fraction_digits++;
+            ifraction = 10 * ifraction + c - '0';
+        }
+    }
+
+    parse_expo:
+
+    if (*buf == 'e') {
+        int expo_minus,ee = 0;
+        buf++;
+        if ((expo_minus = (*buf == '-')))
+            buf++;
+        if (*buf == '+')
+            buf++;
+        while (*buf) {
+            c = *buf++;
+            if (! isdigit(c))
+                goto finish;
+            ee = 10 * ee + c - '0';
+        }
+        // REPLACED IPOW(10,X) BY I10POWX(X) - CLAUDIO MAR 2006
+
+        expo = i10powx(expo_minus ? -ee : ee);
+    }
+
+    finish:
+
+    free(__buf);
+
+    val = expo * ((double) integral + (double) ifraction / i10powx(fraction_digits));
+    return minus ? -val : val;
+}
+
+
+
+
+int parse_expression() {
+    char *expression = equation.expression;
+
+    if(*expression != '{' || expression[strlen(expression) - 1] != '}')
+    {
+        LOGE("Error: not a valid expression\n");
+        return 0;
+    }
+
+    expression += 2; equation.xyz_tokennumber = 0;
+
+    while(*expression != '}') {
+
+        if(equation.xyz_tokennumber >= MAX_TOKEN)
+        {
+            LOGE("Error: too many tokens!\n");
+            return 0;
+        }
+
+        if( isnumber(*expression) || ( *expression == '-' &&  isnumber(* (expression + 1)))) {
+            equation.xyz_token[equation.xyz_tokennumber].value = atof_local(expression);
+            equation.xyz_token[equation.xyz_tokennumber++].type = NUMBER_TYPE;
+            while(*expression != ' ') ++expression; ++expression;
+        }
+
+        else if( isvariable(*expression) && *(expression + 1) == ' ') {
+            int index = *expression - 'a';
+            equation.xyz_token[equation.xyz_tokennumber].index = index;
+            equation.xyz_token[equation.xyz_tokennumber++].type = VARIABLE_TYPE;
+            variable.flag[index] = 1; expression += 2;
+        }
+
+        else if( isunknown(*expression) && *(expression + 1) == ' ') {
+            equation.xyz_token[equation.xyz_tokennumber].index = *expression - 'x';
+            equation.xyz_token[equation.xyz_tokennumber++].type = UNKNOWN_TYPE;
+            expression += 2;
+        }
+
+        else if( isoperator((int) *expression) && *(expression + 1) == ' ') {
+            equation.xyz_token[equation.xyz_tokennumber].index = (int) *expression;
+            equation.xyz_token[equation.xyz_tokennumber++].type = OPERATOR_TYPE;
+            expression += 2;
+        }
+
+        else {
+            int i = 0;
+            while(*expression != ' ') function_name[i++] = *expression++;
+            function_name[i] = '\0'; ++expression;
+            int found = 0;
+            for(i = 0; i < MAX_FUNCTION; i++) {
+                if(!strcmp(function[i].name, function_name)) {
+                    equation.xyz_token[equation.xyz_tokennumber].index = i;
+                    equation.xyz_token[equation.xyz_tokennumber++].type = FUNCTION_TYPE;
+                    found = 1;break;
                 }
+            }
+            if(!found) {LOGE("Error: token unknown in expression: %s\n", function_name);  return 0;}
+        }
+    }
+    return 1;
+}
+
+
+
+
+
+
+
+double equationFunction(double x, double y, double z) {
+    stack_index = -1;
+    for(token = 0; token < equation.xyz_tokennumber; token++) {
+        switch(equation.xyz_token[token].type) {
+            case NUMBER_TYPE:	execution_stack[++stack_index] = equation.xyz_token[token].value;
                 break;
 
-            case VARIABLE_TYPE:
-                execution_stack[++stack_index] = variable.value[equation.xyz_token[token].index];
+            case UNKNOWN_TYPE:	if(equation.xyz_token[token].index == X) execution_stack[++stack_index] = x;
+                else if(equation.xyz_token[token].index == Y) execution_stack[++stack_index] = y;
+                else execution_stack[++stack_index] =z;
                 break;
 
-            case OPERATOR_TYPE:
-                execution_stack[stack_index] = sqrt(execution_stack[stack_index]);
+            case VARIABLE_TYPE:	execution_stack[++stack_index] = variable.value[equation.xyz_token[token].index];
                 break;
 
-            case FUNCTION_TYPE:
-                if (function[equation.xyz_token[token].index].argnum == 1)
+            case OPERATOR_TYPE:	execution_stack[stack_index] = sqrt(execution_stack[stack_index]);
+                break;
+
+            case FUNCTION_TYPE:if(function[equation.xyz_token[token].index].argnum == 1) {
                     execution_stack[stack_index] =
                             function[equation.xyz_token[token].index].function();
+                }
                 else {
                     execution_stack[stack_index - 1] =
                             function[equation.xyz_token[token].index].function();
@@ -193,8 +366,27 @@ double parseEquation(double x, double y, double z) {
                 break;
         }
     }
+
     return execution_stack[stack_index];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 using namespace std;
 
@@ -204,5 +396,9 @@ Java_com_vaca_myapplication_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    equation.expression="{ x 3 + }";
+    parse_expression();
+    double x=equationFunction(2,0,0);
+    hello+=to_string(x);
     return env->NewStringUTF(hello.c_str());
 }
